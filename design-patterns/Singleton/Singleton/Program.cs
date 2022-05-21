1017d0fdb220ed6e30aefbc4e27bf95550460b9a
pea-sys <pea98258@gmail.com>
2022-05-21 17:39:58 +0900

    Singleton

---
+++ design-patterns/Singleton/Singleton/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Singletonパターン：インスタンスが1個しか存在しないことを保証するパターン
+    /// 
+    /// 私はデータベース接続ドライバで使用しています
+    /// 外部リソースへのアクセス等の問題で複数生成されると困るような場合に、使用するといいんではないでしょうか。
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start.");
+            Singleton obj1 = Singleton.getInstance();
+            Singleton obj2 = Singleton.getInstance();
+            if (obj1 == obj2)
+            {
+                Console.WriteLine("obj1とobj2は同じインスタンスです。");
+            }
+            else
+            {
+                Console.WriteLine("obj1とobj2は同じインスタンスではありません。");
+            }
+            Console.WriteLine("End.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ design-patterns/Singleton/Singleton/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Singleton
+    {
+        private static Singleton singleton = new Singleton();
+        private Singleton()
+        {
+            Console.WriteLine("インスタンスを生成しました。");
+        }
+        public static Singleton getInstance()
+        {
+            return singleton;
+        }
+    }
+}
