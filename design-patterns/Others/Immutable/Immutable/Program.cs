b02675351aaf1c59a68f2b21f95d632a6e80e4c3
pea-sys <pea98258@gmail.com>
2022-05-31 20:09:00 +0900

    Immutable

---
+++ design-patterns/Others/Immutable/Immutable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Immutable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Immutableなオブジェクトの作成
+            Rectangle rect = new Rectangle(100, 100);
+            Console.WriteLine($"[{rect.GetType().Name}]Width={rect.Width.ToString()} Height={ rect.Height.ToString()} Area={rect.Area.ToString()}");
+            // 匿名型のオブジェクトでもImmutableになる
+            var anonymous = new {Width = 100, Height = 100, Area= 100 * 100 };
+            Console.WriteLine($"[{anonymous.GetType()}]Width={rect.Width.ToString()} Height={ rect.Height.ToString()} Area={rect.Area.ToString()}");
+
+            Console.Read();
+        }
+    }
+}
