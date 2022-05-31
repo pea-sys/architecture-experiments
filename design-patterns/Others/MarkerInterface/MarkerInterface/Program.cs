d8c9b8d74668958adbd5e7e39c5ea5975c05228e
pea-sys <pea98258@gmail.com>
2022-05-31 21:48:50 +0900

    MarkerInterface

---
+++ design-patterns/Others/MarkerInterface/MarkerInterface/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkerInterface
+{
+    /// <summary>
+    /// MarkerInterfece・・・クラスにメタ情報を付加をするために空のインターフェースを定義する
+    /// 
+    /// 属性で良いような気もする。というのも空のインターフェースは抽象化も行っていないため。
+    /// チーム開発でMarkerInterfaceをうまく運用するのは難しい。秩序より混乱をもたらしそう。
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(10, 100);
+
+        }
+    }
+}
---
+++ design-patterns/Others/MarkerInterface/MarkerInterface/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkerInterface
+{
+    internal class Rectangle : Immutable
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public int Area { get; }
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.Area = width * height;
+        }
+    }
+}
