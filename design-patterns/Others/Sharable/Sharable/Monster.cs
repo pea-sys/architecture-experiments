5b82aec3a5413bc7b49b370067ab0b255154efb0
pea-sys <pea98258@gmail.com>
2022-06-03 22:18:24 +0900

    Sharable

---
+++ design-patterns/Others/Sharable/Sharable/Monster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharable
+{
+    public class Monster
+    {
+        private String name;
+        private int pos;
+        private static Hashtable database = new Hashtable();
+        public static Monster CreateMonster(String name)
+        {
+            lock(database)
+            {
+                Monster creature = (Monster)database[name];
+                if (creature == null)
+                {
+                    creature = new Monster(name);
+                    database.Add(name, creature);
+                }
+                return creature;
+            }
+        }
+        private Monster(String name) { this.name = name; }
+        public void GoRight() { pos++; }
+        public void GoLeft() { pos--; }
+        public String GetName() { return name; }
+        public int GetPos() { return pos; }
+        public override string ToString() { return $"[{name} at {pos}]"; }
+    }
+}
---
+++ design-patterns/Others/Sharable/Sharable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharable
+{
+    /// <summary>
+    /// Sharableパターン・・・オブジェクトを共有するパターン
+    /// 
+    /// [参考URL](https://www.hyuki.com/dp/dpinfo.html#Sharable)
+    /// 状態維持したいオブジェクトを共有する場合に使用
+    /// 大きいオブジェクトや使用頻度の高いオブジェクトを共有する場合は、Flyweightパターン
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Monster mon1 = Monster.CreateMonster("Pikachu");
+            mon1.GoRight();
+            mon1.GoRight();
+            mon1.GoRight();
+
+            Monster mon2 = Monster.CreateMonster("Dodrio");
+            mon2.GoLeft();
+            mon2.GoLeft();
+
+            Monster mon3 = Monster.CreateMonster("Metapod");
+            mon3.GoRight();
+            mon3.GoRight();
+            mon3.GoRight();
+
+            Monster pika = Monster.CreateMonster("Pikachu");
+            Console.WriteLine(pika);
+            Console.ReadLine();
+        }
+    }
+}
