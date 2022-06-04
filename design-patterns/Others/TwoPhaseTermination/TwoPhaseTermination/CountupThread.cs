5f2001c8fba3ff71ac4a9f13cec6b91541ba3624
pea-sys <pea98258@gmail.com>
2022-06-05 08:08:05 +0900

    TwoPhaseTermination

---
+++ design-patterns/Others/TwoPhaseTermination/TwoPhaseTermination/CountupThread.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwoPhaseTermination
+{
+    internal class CountupThread
+    {
+        long _counter = 0;
+        readonly CancellationTokenSource tokenSource;
+        internal CountupThread(CancellationTokenSource tokenSource)
+        {
+            this.tokenSource = tokenSource;
+        }
+        public void ShutdownRequest()
+        {
+            tokenSource.Cancel();
+        }
+
+
+        public Task Task()
+        {
+            return new Task(() => {
+                try
+                {
+                    while (!tokenSource.Token.IsCancellationRequested)
+                    {
+                        DoWork();
+                    }
+                }
+                finally
+                {
+                    DoShutdown();
+                }
+            },tokenSource.Token);
+        }
+
+        void DoWork()
+        {
+            _counter++;
+            Console.WriteLine($"DoWork: counter = { _counter}");
+            Thread.Sleep(1000);
+        }
+
+        void DoShutdown()
+        {
+            Console.WriteLine($"DoShutdown: counter = {_counter}");
+        }
+    }
+}
---
+++ design-patterns/Others/TwoPhaseTermination/TwoPhaseTermination/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwoPhaseTermination
+{
+    /// <summary>
+    /// Two-Phase Termination パターン・・・スレッドの終了処理が終わってからスレッドを止めるパターン
+    /// 
+    /// これは設計というよりは、そうしないとバグが起きるリスクが高くなるよというお話です
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("main: BEGIN");
+
+            
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            var t = new CountupThread(tokenSource);
+            var task = t.Task();
+            task.Start();
+
+            Thread.Sleep(10000);
+
+            Console.WriteLine("main: ShutdownRequest");
+            t.ShutdownRequest();
+
+            Console.WriteLine("main; join");
+            task.Wait();
+
+            Console.WriteLine("main: END");
+            Console.ReadLine();
+        }
+    }
+}
